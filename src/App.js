b8374f11ee00aed62704679e120a8cbfae322284
ktydg <annatars@ktydg>
2024-02-02 15:16:18 +0300

    try again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
 import AppRouter from "./components/AppRouter";
 
